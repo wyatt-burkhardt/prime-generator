15bdf8c12c63707aba68f8d3e18af1cba53263d9
Wyatt <wyatt.burkhardt@gmail.com>
2019-02-10 11:45:03 -0500

    setting up test

--- src/test/resources/com/wyattburkhardt/primegen/PrimeGeneratorEratosthenesTest.java
+++ src/test/resources/com/wyattburkhardt/primegen/PrimeGeneratorEratosthenesTest.java
@@ -12,7 +12,7 @@ public class PrimeGeneratorEratosthenesTest {
 
     @Test
     public void testStandardRange() {
-        
+
         List<Integer> expected = new ArrayList<Integer>();[5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
         PrimeGeneratorEratosthenes generator = new PrimeGeneratorEratosthenes();
